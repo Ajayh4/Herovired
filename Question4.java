9d93217ce3af5e1b8d5c3d20df0d14fe1a701ceb
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2023-02-10 22:48:49 +0530

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,8 +1,8 @@
 import java.util.*;
-class Question48
+class Product
 {
     static Scanner inp=new Scanner(System.in);
-    static void addproducts(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
+    public void addproducts(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
     {
         System.out.println("Enter product name");
         String pn=inp.next();
@@ -21,7 +21,7 @@ class Question48
         pd.add(a);
         pc.add(pq);
     }
-    static void viewproductdetails(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
+    public void viewproductdetails(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
     {
         for(int i=0;i<pl.size();i++)
         {
@@ -33,14 +33,14 @@ class Question48
             System.out.println();
         }
     }
-    static void productlist(ArrayList<String> pl,ArrayList<Integer> pc)
+    public void productlist(ArrayList<String> pl,ArrayList<Integer> pc)
     {
         System.out.println("Enter productname to get count");
         String name=inp.next();
         int k=pl.indexOf(name);
         System.out.println(pl.get(k)+" "+pc.get(k));
     }
-    static void editproduct(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
+    public void editproduct(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
     {
         System.out.println("Enter productname to edit details");
         String p=inp.next();
@@ -59,14 +59,14 @@ class Question48
         pd.set(k, a);
         pc.set(k,pq);
     }
-    static void productcount(ArrayList<String> pl,ArrayList<Integer> pc)
+    public void productcount(ArrayList<String> pl,ArrayList<Integer> pc)
     {
         System.out.println("Enter productname to get count");
         String pn=inp.next();
         int i=pl.indexOf(pn);
         System.out.println(pl.get(i)+"    "+pc.get(i));
     }
-    static void updateinventory(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
+    public void updateinventory(ArrayList<String> pl,ArrayList<ArrayList<String>> pd,ArrayList<Integer> pc)
     {
         System.out.println("Enter 1 to add and 2 to delete product quantity");
         int choice=inp.nextInt();
@@ -93,14 +93,20 @@ class Question48
             pd.get(i).set(2,Integer.toString(m));
         }
     }
+}
+class Que4
+{
     public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        Product obj=new Product();
         ArrayList<String> pl=new ArrayList<>();
         ArrayList<ArrayList<String>> pd=new ArrayList<>();
         ArrayList<Integer> pc=new ArrayList<>();
         System.out.println("Enter Number of items to be added");
         int n=inp.nextInt();
         for(int i=0;i<n;i++)
-        addproducts(pl,pd,pc);
-        viewproductdetails(pl, pd, pc);     
-    } 
+        obj.addproducts(pl,pd,pc);
+        obj.viewproductdetails(pl, pd, pc);
+        obj.productcount(pl, pc);
+    }
 }
